524ce8bdff3f9cfa4750e39c098363aefee434a0
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2022-12-18 22:03:53 +0530

    update:- implement new array method

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,12 @@
-console.log("Check");
+let arr = [1,2,4, 5, 6, 7, 13,3,5,6,24];
+
+// calculate length of array
+
+// console.log(arr.length);
+
+// console.log(arr.reverse());
+
+// console.log(arr.sort());
+console.log(arr);
+console.log(arr.pop());
+console.log(arr);
