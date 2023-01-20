28113379965bdfa6727bd2cc0a75216e83890f09
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-01-20 11:42:38 +0530

    learn:- single object to create and access

---
+++ JS/objectExample.js
@@ -0,0 +1,8 @@
+let aktuCollegeList = {
+    collegeID: 114,
+    collegeName: 'JSS Academy',
+    collegeLocation: 'Ghaziabd',
+}
+
+console.log(`Hello Guest! \n We know college name of Affilate to AKTU college id  
+ "${aktuCollegeList.collegeID}"  \n College Name is "${aktuCollegeList.collegeName}" and \n College Location "${aktuCollegeList.collegeLocation}" `);
