dcf7ee796add7221f2e02ae81261d1a9c41633cd
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-08 17:57:15 +0530

    feat:- Update the object Example

--- JS/objectExample.js
+++ JS/objectExample.js
@@ -1,8 +1,19 @@
-let aktuCollegeList = {
-    collegeID: 114,
-    collegeName: 'JSS Academy',
-    collegeLocation: 'Ghaziabd',
-}
+// let aktuCollegeList = {
+//     collegeID: 114,
+//     collegeName: 'JSS Academy',
+//     collegeLocation: 'Ghaziabd',
+// }
 
-console.log(`Hello Guest! \n We know college name of Affilate to AKTU college id  
- "${aktuCollegeList.collegeID}"  \n College Name is "${aktuCollegeList.collegeName}" and \n College Location "${aktuCollegeList.collegeLocation}" `);
+// console.log(`Hello Guest! \n We know college name of Affilate to AKTU college id  
+//  "${aktuCollegeList.collegeID}"  \n College Name is "${aktuCollegeList.collegeName}" and \n College Location "${aktuCollegeList.collegeLocation}" `);
+
+// Passing multiple date and use them this object
+
+ aktuCollegeList (collegeID,collegeName, collegeLocation)[{
+    this.collegeID;
+    this.collegeName;
+    this.collegeLocation;
+}]
+
+let list = new aktuCollegeList (113,'JSS', 'Ghazibad');
+console.log(list);
