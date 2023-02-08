1a72750076bb9ed4099b6a09b885a7c0ca062ce6
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-08 17:57:38 +0530

    adding array

---
+++ JS/array.js
@@ -0,0 +1,26 @@
+// let arr = [1,2,4, 5, 6, 7, 13,3,5,6,24];
+
+// calculate length of array
+
+// console.log(arr.length);
+
+// console.log(arr.reverse());
+
+// console.log(arr.sort());
+// console.log(arr);
+// console.log(arr.pop());
+// console.log(arr);
+
+
+var schoolName = ["IMSGhaziabad", "AKEGC", "JSS", "KIET", "RKGIET", "NIET", "GLBAJAJ", "ACCURATECOLLEGE"];
+// console.log(schoolName);
+// console.log(schoolName.length);
+// console.log(schoolName.sort());
+// console.log("Hello")
+// console.log(schoolName.pop());
+// console.log(schoolName);
+// schoolName.forEach(element => console.log(element));
+// schoolName.forEach(name => console.log(r))
+console.log(schoolName);
+schoolName[0] = "Ashish";
+console.log(schoolName);
