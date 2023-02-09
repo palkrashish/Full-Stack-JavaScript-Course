ad1b6a408058fa432d9d207b2bca9012057b30a5
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-09 10:38:06 +0530

    feat :+1: celsiusExample

---
+++ JS/01Basic/celsius.js
@@ -0,0 +1,30 @@
+var score  = 100
+score = 130
+
+const bonus = 50
+
+
+var finalScore =  (score +bonus) * 1.9;
+
+var complex = ((4 + 4) * (5 -1)) /  2;
+
+// What is use of  moudules % .
+
+var exampleMoudles = 15;
+
+let result = exampleMoudles % 2;
+console.log(result);
+
+
+console.log(finalScore);
+
+console.log(complex);
+
+var tempInFahrenheit =  100;
+
+// do calculation  
+// (32°F − 32) × 5/9 = 0°C
+
+let celcius = (tempInFahrenheit - 32) * 5/9
+
+console.log("Now, We can see degree:" ,celcius);
