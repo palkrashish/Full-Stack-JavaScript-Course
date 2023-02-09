ba86c0d267563d7823f837bf064bf174852253e0
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-09 10:15:35 +0530

    feat :- how many way use the variable

---
+++ JS/01Basic/variable.js
@@ -0,0 +1,16 @@
+// console.log(b); // ReferenceError
+// console.log(a); // undefined
+// var a = 10;
+// console.log(a); // 10
+// let b = 20;  
+// console.log(b);
+
+// const c = 30;
+// console.log(c);
+
+let nameFirst = "Ashish"
+let lastName = "Kr Pal"
+// console.log(nameFirst);  // Ashish
+
+// console.log("My completed name is: " + nameFirst + lastName);  // My completed name is: Ashish Kr Pal
+console.log("There is another way you can :" + nameFirst + ' ' + lastName);
