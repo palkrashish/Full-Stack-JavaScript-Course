a633c47eef7701b044d5b37b269c065ec02dbc4c
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-09 13:26:04 +0530

    array and Template

---
+++ JS/02Basic/marvel.js
@@ -0,0 +1,15 @@
+// array example
+
+const superHero = ['Iron Man', 'Spiderman', 'Capt. America'];
+
+console.log(superHero);
+console.log(superHero[0]);
+console.log(superHero[2]);
+
+console.log(superHero.length);
+console.log(superHero[superHero.length-1]);
+// example of template
+console.log(`We have ${superHero.length} super hero`);
+
+
+
