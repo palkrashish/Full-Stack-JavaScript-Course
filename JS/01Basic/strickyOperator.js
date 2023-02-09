30b0b16287064219eccf5d5406b0683335d45eb3
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-09 10:52:36 +0530

    feat :+1:  stricky ( == and === )

---
+++ JS/01Basic/strickyOperator.js
@@ -0,0 +1,15 @@
+
+var num = 12
+var num1 = 12;
+var num1 = "12";
+if ( num == num1){
+    console.log("Number is same"); //  Number is Same
+}
+else 
+console.log(" NOT the same number");
+
+if ( num === num1){
+    console.log("Number is same"); 
+}
+else 
+console.log(" NOT the same number"); // NOT the same number
