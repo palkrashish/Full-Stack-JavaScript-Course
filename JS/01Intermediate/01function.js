a2817cdaf4b6ce9ef866c95e09682cf223c6a8a2
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-10 11:30:09 +0530

    function example basic

---
+++ JS/01Intermediate/01function.js
@@ -0,0 +1,16 @@
+let sayHello = function (name) {
+    console.log("Welcome to my Page");
+    console.log(`Hey ${name} `);
+}
+
+// sayHello("Ashish")
+
+
+let fullNameStudent = function (firstName, lastName) {
+
+    console.log(" Welcome to my Instagram Page ");
+    console.log(` ${ firstName } ${ lastName }`);
+}
+
+
+fullNameStudent('Ashish', 'Pal')
