ab1cb6d56d9dba9421588e71d0c3b52b49cd57b0
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-10 11:54:32 +0530

    update: default parameter

--- JS/01Intermediate/01function.js
+++ JS/01Intermediate/01function.js
@@ -13,4 +13,24 @@ let fullNameStudent = function (firstName, lastName) {
 }
 
 
-fullNameStudent('Ashish', 'Pal')
+// fullNameStudent('Ashish', 'Pal')
+
+let multiPlayer = function (num, num1){
+
+    // let multi = num * num1;
+    // return multi;
+
+    return num * num1;
+}
+
+// console.log(multiPlayer(2,3));  // 6
+
+// DEFAULT PARAMETER EXAMPLE
+
+
+ let exampleDef = function ( name,  mobileNo) {
+    return  ' Hello ' + name +  ' and you Mobile number is: ' + mobileNo
+ }
+
+ 
+ console.log( exampleDef('Ashish', 95876876456567)); // Hello Ashish and you Mobile number is: 95876876456567
