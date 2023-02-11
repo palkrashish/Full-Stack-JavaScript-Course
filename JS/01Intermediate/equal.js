ec3c27217cfe60aad66a32c9b6ce69c8005dec0c
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-11 23:16:23 +0530

    equal operator == and === to check

---
+++ JS/01Intermediate/equal.js
@@ -0,0 +1,19 @@
+// console.log( 2 === 2.00); // true
+// console.log(2 == 2.0); // true
+
+// let myCode = '';
+
+// let myCode1 = '';
+
+
+// console.log(myCode == myCode1); // true
+
+// console.log( myCode === myCode1); // true
+
+let textStr = {}
+// let textStr = {}
+let textStr1 = ''
+
+// console.log(textStr == textStr1); // false
+console.log(textStr === textStr1); // false
+console.log(textStr == textStr1); // false
