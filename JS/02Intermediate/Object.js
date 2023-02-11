4f83c66e618facbab3cc58ac68c0815a8e8268b4
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-11 23:49:25 +0530

    feat:- example of Object

---
+++ JS/02Intermediate/Object.js
@@ -0,0 +1,11 @@
+let myYoutubeVideos = {
+    title: 'JavaScript',
+    videoLength: 15,
+    authorBy: 'Hitesh Chodhary',
+    videoDescription: 'Basic of JavaScript',
+    priceCourse: '299 Rs.'
+}
+
+// console.log(myYoutubeVideos);
+
+console.log(`Hey User!, There is new course  "${myYoutubeVideos.videoDescription}" would you like purchase "${ myYoutubeVideos.priceCourse}" `);
