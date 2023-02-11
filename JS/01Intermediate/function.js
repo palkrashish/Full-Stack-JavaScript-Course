3d79832078604e4df1cf4060630a15e065e214d0
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-11 13:27:31 +0530

    code refactor

--- JS/01Intermediate/function.js
+++ JS/01Intermediate/function.js
@@ -1,35 +1,23 @@
-const marksNumber = function (currentMarks = 0, totalMarks =0) {
-    let  myPercent = (currentMarks/totalMarks) *100;
+const marksNumber = function (currentMarks = 0, totalMarks = 0) {
+  let myPercent = (currentMarks / totalMarks) * 100;
 
-    let myGrade = ''
+  let myGrade = "";
 
+  if (myPercent >= 90) {
+    // console.log("Grade A");
+    myGrade = "A";
+  } else if (myPercent >= 75) {
+    // console.log( "Grade B");
+    myGrade = "B";
+  } else if (myPercent >= 60) {
+    // console.log("Grade C");
+    myGrade = "C";
+  }
+  // {console.log("Fail");}
+  else myGrade = "D";
 
-    if (myPercent >= 90) {
-        // console.log("Grade A");
-        myGrade = 'A'
-        
-    }
-    else if (myPercent >= 75) {
-        // console.log( "Grade B");
-        myGrade = 'B'
+  return `Your grade is ${myGrade} and Percentage is ${myPercent}`;
+};
 
-    }
-    else if (myPercent >= 60) {
-        // console.log("Grade C");
-        myGrade = 'C'
-
-        
-    }else  
-        // {console.log("Fail");}
-        myGrade = 'D'
-
-        
-    return `Your grade is ${myGrade} and Percentage is ${myPercent}`
-
-
-}
-
- let yourResult = marksNumber(80,100) 
-    console.log(yourResult);
-
-
+let yourResult = marksNumber(80, 100);
+console.log(yourResult);
