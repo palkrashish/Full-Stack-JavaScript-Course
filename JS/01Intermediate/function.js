f0dcc5062cb08dd62dcb101e1e4f9fad21effdbf
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-11 13:26:01 +0530

    make a project grading system with the help of function

---
+++ JS/01Intermediate/function.js
@@ -0,0 +1,35 @@
+const marksNumber = function (currentMarks = 0, totalMarks =0) {
+    let  myPercent = (currentMarks/totalMarks) *100;
+
+    let myGrade = ''
+
+
+    if (myPercent >= 90) {
+        // console.log("Grade A");
+        myGrade = 'A'
+        
+    }
+    else if (myPercent >= 75) {
+        // console.log( "Grade B");
+        myGrade = 'B'
+
+    }
+    else if (myPercent >= 60) {
+        // console.log("Grade C");
+        myGrade = 'C'
+
+        
+    }else  
+        // {console.log("Fail");}
+        myGrade = 'D'
+
+        
+    return `Your grade is ${myGrade} and Percentage is ${myPercent}`
+
+
+}
+
+ let yourResult = marksNumber(80,100) 
+    console.log(yourResult);
+
+
