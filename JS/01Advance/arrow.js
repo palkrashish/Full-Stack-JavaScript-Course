d4bb5a9c9928550d82cc7e679a101a7bb203c16a
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-14 11:56:47 +0530

    update: with arrow function

--- JS/01Advance/arrow.js
+++ JS/01Advance/arrow.js
@@ -9,4 +9,41 @@ const exampleHello = (name) => {
   return ` Hey "${name}" , Welcome to my Page!`;
 };
 
-console.log(exampleHello("Ashish"));
+// console.log(exampleHello("Ashish")); // Hey "Ashish" , Welcome to my Page!
+
+const todos =  [
+{
+    title: 'Buy a Amul Milk',
+    isDone: true,
+},
+ {
+    title: 'Sugger',
+    isDone: true,
+
+ },
+ {
+    title: 'Haldiram namkeen',
+    isDone: false   
+ }
+]
+
+// const resultTodo = todos.filter( (todo) =>  {
+//     return todo.isDone === true;
+    
+// } ) 
+
+const resultTodo = todos.filter( (todo) => todo.isDone === true ) 
+// Same out put line 30
+// [
+//     { title: 'Buy a Amul Mile', isDone: true },
+//     { title: 'Sugger', isDone: true }
+//   ]
+// console.log(resultTodo);
+
+const resultTitle = todos.filter( (resultT) => resultT.title == 'Buy a Amul Milk')
+// const resultTitle1 = todos.filter( (resultT) => resultT.title === 'Buy a Amul Milk' )
+
+console.log(resultTitle);
+// console.log(resultTitle1);
+
+
