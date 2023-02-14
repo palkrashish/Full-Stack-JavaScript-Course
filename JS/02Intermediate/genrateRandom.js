7b0f5fa49accc4cc1b6ad758ffcffee3c26dd571
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-14 11:00:59 +0530

    feat: - generateRandome number

---
+++ JS/02Intermediate/genrateRandom.js
@@ -0,0 +1,18 @@
+let number = 40;
+let number1 = 14;
+
+let result =  number/number1;
+
+// console.log(result.toFixed(2)); // print the number after decimal 2 values.  likee 2.86
+
+let upperBound = 25;
+
+let lowerBound = 20;
+
+let myRandom = (Math.random()  * (upperBound - lowerBound +1 )) +   lowerBound
+
+
+// When we are adding '+ lowerBound' means that we are expection 20 above random number
+console.log(myRandom);
+
+
