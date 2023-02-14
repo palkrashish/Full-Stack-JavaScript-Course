15b3361b093811ecf89da498d8548c44264ea215
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-14 15:25:22 +0530

    using this keyword

---
+++ JS/01Advance/arrowthiskeyword.js
@@ -0,0 +1,11 @@
+const gadgetMine = {
+    camera: 'Canon',
+    price: 600000,
+    Dscrption : function ()  {
+        return `this price of camera ${this.price} `
+        
+    }
+}
+
+console.log(gadgetMine.Dscrption());
+
