92b2435a209fdb23e354ba48c82c3f069fff94e8
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-17 12:15:22 +0530

    how to select all selectior

--- JS/01WebBasics/script.js
+++ JS/01WebBasics/script.js
@@ -1,6 +1,21 @@
 // alert("Hello I'm Ashish");
-console.log(document);
-console.log(document.body);
-console.log(document.title);
-console.log(document.width  );
-console.log(document.documentURI);
+// console.log(document);
+// console.log(document.body);
+// console.log(document.title);
+// console.log(document.width  );
+// console.log(document.documentURI);
+
+
+// console.log(document.title);
+// document.title = "I got changed" // with the help this title will be change
+
+// const myResult = document.getElementById('idone');
+const myResult1 = document.getElementsByClassName('classone');
+// const myResult = document.querySelectorAll('p');
+let myResult = document.querySelector('#idone')
+console.log(myResult);
+// console.log(myResult1);
+
+
+
+
