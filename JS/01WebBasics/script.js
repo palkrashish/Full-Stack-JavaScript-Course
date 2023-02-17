570e72b17034bfb0f6f11078d01a9db97b4c6930
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-02-17 14:01:07 +0530

    update: - changing text using forEach method

--- JS/01WebBasics/script.js
+++ JS/01WebBasics/script.js
@@ -16,12 +16,19 @@
 // document.querySelectorAll
 
 // const myResult = document.getElementById('idone');
-const myResult1 = document.getElementsByClassName('classone');
-// const myResult = document.querySelectorAll('p');
-let myResult = document.querySelector('#idone')
-console.log(myResult);
-// console.log(myResult1);
+// const myResult1 = document.getElementsByClassName('classone');
+// // const myResult = document.querySelectorAll('p');
+// let myResult = document.querySelector('#idone')
+// console.log(myResult);
+// // console.log(myResult1);
 
+//  CHANGING ALL TEXT USING LOOP
 
+// const changeResult = document.querySelector('p')
+// changeResult.textContent =' I am changed text '
 
+const changeResult = document.querySelectorAll('p');
 
+changeResult.forEach( function(p){
+    p.textContent = 'Now, all text changed using for each method'
+} )
