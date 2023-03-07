36370bb76b1913cb9ef7e625aa88103ca9598604
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-03-07 22:21:57 +0530

    feat : - Example of DOM

---
+++ JS/DOM/01basic/script.js
@@ -0,0 +1,8 @@
+// console.log('Hello')
+// console.log(document)
+// console.log(window)
+// console.log(window.document)// line 3 and 4 are same out beacuase we can only document automatic injected
+// console.log(document.documentElement)
+// console.log(document.head)
+console.log(document.body)
+console.log(document.body.firstChild)
