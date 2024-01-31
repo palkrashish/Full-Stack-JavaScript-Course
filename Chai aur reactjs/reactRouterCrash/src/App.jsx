d0358c09f8566ed9c3ae46ede6de1cff23db3151
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-02-01 00:49:39 +0530

    feat:- using react router page design

--- Chai aur reactjs/reactRouterCrash/src/App.jsx
+++ Chai aur reactjs/reactRouterCrash/src/App.jsx
@@ -1,11 +1,8 @@
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-
 function App() {
   return (
     <>
-      <Header />
-      <Footer />
+    <h5>Loading..</h5>
+    
     </>
   );
 }
---
+++ Chai aur reactjs/reactRouterCrash/src/components/Github/Github.jsx
@@ -0,0 +1,10 @@
+const Github = () => {
+  return (
+    <>
+ <h3 className="bg-graay-400" ></h3>
+      
+    </>
+  )
+}
+
+export default Github
